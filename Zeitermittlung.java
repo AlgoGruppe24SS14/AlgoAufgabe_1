cb3913bcfb11b649558c5c6e52de444d6131b4eb
praKtastic <pascal.hintze@gmx.net>
2014-03-25 12:46:44 +0100

    New class Zeitermittlung + Methoden hinzugefügt

---
+++ Zeitermittlung.java
@@ -0,0 +1,14 @@
+package de.ostfalia.algodat.ss14.ScheinSprache;
+import de.ostfalia.algodat.ss14.ScheinSprache.ScheinSprache;
+
+public class Zeitermittlung {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
